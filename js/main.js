446912ae477ff92ef87739de096c561163e89486
Sam <srgrosenick@gmail.com>
2016-05-09 09:27:52 -0800

    changed basemaps to ESRI maps

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 var map = L.map('map').setView([61.67, -149.07], 7);
-        mapLink = 
-            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; ' + mapLink,
-            maxZoom: 18,
-            }).addTo(map);
+var layer = L.esri.basemapLayer('Topographic').addTo(map);
+var layerLabels;
 
-//var peecture = new L.Icon.Default();
+function setBasemap(basemap) {
+    if (layer) {
+      map.removeLayer(layer);
+    }
+
+    layer = L.esri.basemapLayer(basemap);
+
+    map.addLayer(layer);
+
+    if (layerLabels) {
+      map.removeLayer(layerLabels);
+    }
+
+    if (basemap === 'ShadedRelief'
+     || basemap === 'Oceans'
+     || basemap === 'Gray'
+     || basemap === 'DarkGray'
+     || basemap === 'Imagery'
+     || basemap === 'Terrain'
+   ) {
+      layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+      map.addLayer(layerLabels);
+    }
+  }
+
+  function changeBasemap(basemaps){
+  var basemap = basemaps.value;
+  setBasemap(basemap);
+  }
+
 
 function createPopup (feature, layer) {
     layer.bindPopup(feature.properties.place + " <a href='"+feature.properties.theweb+"'>" + feature.properties.theweb + "</a>");
